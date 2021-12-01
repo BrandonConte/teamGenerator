98e685a5455019c712598b0e3917d1d62383047b
BrandonConte <bconte950@gmail.com>
2021-12-01 18:19:25 -0500

    imported modules

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const Team = require('./lib/Team');
 
-new Team().intializeTeam();
+const generatePage = require('./src/page-temp');
+const writeFile = require('./utils/generate-site');
+
+new Team().intializeManager();
